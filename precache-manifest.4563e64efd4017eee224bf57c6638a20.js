2acc85a6b726264e1290ca054b0d19d27a5d2d80
Apagyi Dávid <apagyi.david@gmail.com>
2019-12-28 23:01:26 +0100

    Updates

---
+++ precache-manifest.4563e64efd4017eee224bf57c6638a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af886bb150a8ad871df428f34a9368a",
+    "url": "/logigrafika/index.html"
+  },
+  {
+    "revision": "ce3f6e62383eb0933686",
+    "url": "/logigrafika/static/css/main.c49f564f.chunk.css"
+  },
+  {
+    "revision": "c3646eaf1f5941f491b5",
+    "url": "/logigrafika/static/js/2.79ff43b9.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/logigrafika/static/js/2.79ff43b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce3f6e62383eb0933686",
+    "url": "/logigrafika/static/js/main.425cf962.chunk.js"
+  },
+  {
+    "revision": "ae9b0949079d0aa2b210",
+    "url": "/logigrafika/static/js/runtime-main.5fd55e16.js"
+  }
+]);
